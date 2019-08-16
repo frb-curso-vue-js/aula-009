27b4c0814494b3f206e456f958a2e6895c48e163
FRamos <framos.df@gmail.com>
2019-08-16 17:38:10 -0300

    Criando repositório. FRB

---
+++ src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <!-- {{ citacao }} -->
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        citacao: String
+    }
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
